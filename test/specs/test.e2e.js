675b7b0cb905e9c91ced563ecd6083bd17232fe6
Murali Krishnamurthy <murali.krishnamurthy@blueacornici.com>
2024-08-15 12:24:02 -0400

    keep allure report to 30 days and remove e2e.js error

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,13 +1,9 @@
-import { expect } from '@wdio/globals'
-import LoginPage from '../pageobjects/login.page.js'
-import SecurePage from '../pageobjects/secure.page.js'
 
 describe('Test Script Github Actions', () => {
     it('Open Toyota Autoparts', async () => {
         await browser.url('https://autoparts.toyota.com/')
         await $("//a[contains(text(),'Shop Wheels')]").click()
-        await $("h1.plp__title").waitForClickable()
-        await $('.span').click()
+        await $("h1.plp__title").waitForClickable()        
     })
 })
 
